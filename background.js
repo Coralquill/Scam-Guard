3609d359ccec7ae8eba1eb7aeba54cbb00fcfb73
Coralquill <sujal9107@gmail.com>
2025-12-18 02:49:47 +0530

    Complete end-to-end extension to backend communication

--- background.js
+++ background.js
@@ -1,13 +1,18 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (!message || !message.text) {
+      sendResponse({ error: "No text provided" });
+      return true;
+    }
+  
     fetch("http://127.0.0.1:8000/analyze", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text: message.text })
     })
-    .then(res => res.json())
-    .then(data => sendResponse(data))
-    .catch(err => sendResponse({ error: err.toString() }));
+      .then(res => res.json())
+      .then(data => sendResponse(data))
+      .catch(err => sendResponse({ error: err.toString() }));
   
-    return true; // keep message channel open
+    return true;
   });
   
